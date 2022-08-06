0570dd279acbc86e69d2a425b42a2c5c0fc12fdb
Victor Galkin <vgalkin@itkey.com>
2022-08-06 23:41:16 +0300

    add deployer account

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,10 +2,13 @@ from brownie import Sketch, Factory, accounts, interface
 # export POLYGONSCAN_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXX___FIXME
 # brownie run scripts/deploy.py --network polygon-main
 def main():
+    deployer = accounts.load('polygon_deployer')
     sketchContract = Sketch.deploy(
+        {'from': deployer},
         publish_source=True,
     )
 
     factoryContract = Factory.deploy(
+        {'from': deployer},
         publish_source=True,
     )
