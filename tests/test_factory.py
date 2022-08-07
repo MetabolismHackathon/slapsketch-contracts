7bd641bdd4a6339612c5d3d2d1ddfa18e1116b65
Victor Galkin <vgalkin@itkey.com>
2022-08-07 17:24:53 +0300

    test for evaluatePieces

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -61,3 +61,34 @@ def test_create_collab(factory, sketch, accounts, Collab):
     print(f"signature={signature}")
     Collab.at(collab_address).setupPermitted(signature, {"from": local})
 
+@pytest.mark.require_network("polygon-main-fork-alchemy")
+def test_evaluate_pieces(factory, sketch, accounts, Collab):
+    local = accounts.add(private_key="0x416b8a7d9290502f5661da81f0cf43893e3d19cb9aea3c426cfb36e8186e9c09")
+    factory.setSketch(sketch)
+    sketch.setFactory(factory)
+    sketch_id = sketch.startSketch(
+        "ipfs:///bafyreichzhlqfew2prvc6hpx2ug5lxn25sfwutwlx3iby64r46ztl2wgme/metadata.json",
+        {"from": local}
+    ).return_value
+
+    collab_address, sign_message = factory.createCollab(sketch_id, "Name", 2, 2, {"from": local}).return_value
+    icollab = interface.ICollab(collab_address)
+    icollab.evaluatePieces([3,2,1,0], [3,2,1,0], {"from": local})
+    icollab.evaluatePieces([0,1,2,3], [0,1,2,3], {"from": local})
+    print(f"collab_address={collab_address}")
+    collab_contract = Collab.at(collab_address)
+    assert [1,1,1,1] == [
+        collab_contract.totalUpvotes(0),
+        collab_contract.totalUpvotes(1),
+        collab_contract.totalUpvotes(2),
+        collab_contract.totalUpvotes(3),
+    ]
+    assert [1,1,1,1] == [
+        collab_contract.totalDownvotes(0),
+        collab_contract.totalDownvotes(1),
+        collab_contract.totalDownvotes(2),
+        collab_contract.totalDownvotes(3),
+    ]
+
+     
+
